add0627a665ca9ba9b64a9e6aeed0acb2753c57d
Morris <underwater_forests@outlook.com>
2026-08-29 17:38:22 -0400

    Pushing code to cloud to code from laptop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Immedaite Invocked Function Expression (IIFEs)
 const gameboard = (() => {
     // Creats fresh board
-    let board = ['','','','','','','','',''];
+    let board = ['1','2','3','4','5','6','7','8','9'];
  
     const resetBoard = () =>  { 
         board = ['','','','','','','','',''];
@@ -9,8 +9,8 @@ const gameboard = (() => {
   
     const getBoard = () => board;
 
-    const setBoard = (index, mark) => {
-        board[index] = mark;
+    const setBoard = (index, marker) => {
+        board[index] = marker;
     }
 
     return { resetBoard, getBoard, setBoard }
@@ -18,7 +18,7 @@ const gameboard = (() => {
 
 // Player Factory Function
 function player(name, marker) {
-    let userChoice = "";
+    let userChoice = "9";
     return {
         name,
         marker,
@@ -26,14 +26,18 @@ function player(name, marker) {
     };
 };
 
-
-
 // Game flow Driver
 (() => {
     // Start game
+    console.log('This is a visual representation of Tic-Tac-Toe board.\n\nPlease select your maker to use it.\n\nInput the digit where you would want to place your marker.')
     // Visual Board
-    const visualBoard = "0|1|2\n-+-+-\n3|4|5\n-+-+-\n6|7|8\n";
+    const visualBoard = "1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n";
     console.log(visualBoard);
+    console.log(gameboard.getBoard());
+    player('morris', 'X');
+    console.log(gameboard.getBoard());
+
+    // I can update the visual board on screen which is a screen for every marker selected. Remember to create logic to not allow more than one marker be selected for a certain spot
 
     // Game logic for play
 
